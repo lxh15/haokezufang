28b2fd3d24716b7b3990af5dc57d794e73434f4c
lxh15 <1392649188@qq.com>
2022-07-13 19:46:04 +0800

    准备工作完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false // 第一行报错
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/no-duplicates': 0,
+    'vue/multi-word-component-names': 'off'
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入字体图标
+import '@/assets/fonts/iconfont.js'
+// 引入want 组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 引入flexble
+import 'amfe-flexible/index.min.js'
+
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
